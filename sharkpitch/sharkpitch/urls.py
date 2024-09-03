f56c111d6cd7b50f5323d17b77d1ef4d4230a4f5
Amudala Premendar Reddy <premamudala2@gmail.com>
2024-09-03 17:01:11 +0530

    path changed

--- sharkpitch/sharkpitch/urls.py
+++ sharkpitch/sharkpitch/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about',include('about.html')),
 ]
