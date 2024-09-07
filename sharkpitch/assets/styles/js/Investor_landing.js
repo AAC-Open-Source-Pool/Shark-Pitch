0fd69a407f6c15091e584bbcf8aec17a9d6750da
Amudala Premendar Reddy <premamudala2@gmail.com>
2024-09-07 23:46:16 +0530

    js for investor landing page

--- sharkpitch/assets/styles/js/Investor_landing.js
+++ sharkpitch/assets/styles/js/Investor_landing.js
@@ -1,5 +1,5 @@
-const videos = document.querySelectorAll('.clickable-video');
-videos.forEach(video => {
+const Videos = document.querySelectorAll('.clickable-video');
+Videos.forEach(video => {
     video.addEventListener('click', function() {
         if (video.paused) {
             video.play();
@@ -17,8 +17,28 @@ document.getElementById('search-bar').addEventListener('input', function() {
         const startup = item.getAttribute('data-startup').toLowerCase();
         if (startup.includes(searchQuery)) {
             item.style.display = ''; 
-        } else {
-            item.style.display = 'none'; 
+        } 
+        else {
+            item.style.display = 'none';
         }
+    });
+});
+
+
+const categoryButtons = document.querySelectorAll('.category');
+const videos = document.querySelectorAll('.clickable-video');
+
+categoryButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        const selectedCategory = this.getAttribute('data-category');
+
+        videos.forEach(video => {
+            const videoCategory = video.getAttribute('data-tag').toLowerCase();
+            if (videoCategory === selectedCategory) {
+                video.style.display = ''; 
+            } else {
+                video.style.display = 'none';
+            }
+        });
     });
 });
