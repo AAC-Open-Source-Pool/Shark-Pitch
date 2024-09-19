5494d5d2d66f7134f1f0ddd625e41aa4696252d8
Raag-24 <raag.chekuri@gmail.com>
2024-09-19 23:35:46 +0530

    added mainapp and converted css and image files to static

--- sharkpitch/mainapp/urls.py
+++ sharkpitch/mainapp/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from . import views 
+
 urlpatterns = [
-    path('',views.home,name='home')
+    path('', views.login, name='home'),
+    path('signup/', views.sign_up, name='sign_up'),
+    path('startup-verification/', views.startup_verification, name='startup_verification'),
+    path('investor-description/', views.investor_description, name='investor_description'),
 ]
--- sharkpitch/mainapp/views.py
+++ sharkpitch/mainapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return render(request,'homepage.html')
+def login(request):
+    return render(request, 'login.html')
+
+def sign_up(request):
+    return render(request, 'sign_up_page.html')
+
+def startup_verification(request):
+    return render(request, 'startup_verification_page.html')
+
+def investor_description(request):
+    return render(request, 'investor_description_page.html')
