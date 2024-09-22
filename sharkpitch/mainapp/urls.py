8cdb05997888e25b0ffe38054c1d833c2b27787d
Raag-24 <raag.chekuri@gmail.com>
2024-09-22 23:00:46 +0530

    Fully working frontend webpages in the mainapp

--- sharkpitch/mainapp/urls.py
+++ sharkpitch/mainapp/urls.py
@@ -3,8 +3,19 @@ from django.urls import path, include
 from . import views 
 
 urlpatterns = [
-    path('', views.login, name='home'),
-    path('signup/', views.sign_up, name='sign_up'),
-    path('startup-verification/', views.startup_verification, name='startup_verification'),
-    path('investor-description/', views.investor_description, name='investor_description'),
+    path('login', views.login, name='login'),
+    path('sign_up', views.sign_up, name='sign_up'),
+
+    path('startup_verification_page1', views.startup_verification1, name='startup_verification1'),
+    path('startup_verification_page2', views.startup_verification2, name='startup_verification2'),
+    path('startup_verification_page3', views.startup_verification3, name='startup_verification3'),
+    path('startup_verification_page4', views.startup_verification4, name='startup_verification4'),
+    path('startup_verification_page5', views.startup_verification5, name='startup_verification5'),
+    path('startup_verification_page6', views.startup_verification6, name='startup_verification6'),
+    path('startup_verification_page7', views.startup_verification7, name='startup_verification7'),
+    path('startup_verification_page8', views.startup_verification8, name='startup_verification8'),
+    path('startup_verification_page9', views.startup_verification9, name='startup_verification9'),
+    path('startup_verification_page10', views.startup_verification10, name='startup_verification10'),
+
+    path('investor_description', views.investor_description, name='investor_description'),
 ]
--- sharkpitch/mainapp/views.py
+++ sharkpitch/mainapp/views.py
@@ -7,8 +7,35 @@ def login(request):
 def sign_up(request):
     return render(request, 'sign_up_page.html')
 
-def startup_verification(request):
-    return render(request, 'startup_verification_page.html')
+def startup_verification1(request):
+    return render(request, 'startup_verification_page1.html')
+
+def startup_verification2(request):
+    return render(request, 'startup_verification_page2.html')
+
+def startup_verification3(request):
+    return render(request, 'startup_verification_page3.html')
+
+def startup_verification4(request):
+    return render(request, 'startup_verification_page4.html')
+
+def startup_verification5(request):
+    return render(request, 'startup_verification_page5.html')
+
+def startup_verification6(request):
+    return render(request, 'startup_verification_page6.html')
+
+def startup_verification7(request):
+    return render(request, 'startup_verification_page7.html')
+
+def startup_verification8(request):
+    return render(request, 'startup_verification_page8.html')
+
+def startup_verification9(request):
+    return render(request, 'startup_verification_page9.html')
+
+def startup_verification10(request):
+    return render(request, 'startup_verification_page10.html')
 
 def investor_description(request):
     return render(request, 'investor_description_page.html')
