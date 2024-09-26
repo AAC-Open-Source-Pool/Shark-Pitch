ddce8afbd72ed0d013f5f91b3cff9e548a15808a
Amudala Premendar Reddy <premamudala2@gmail.com>
2024-09-26 19:10:52 +0530

    urls mainapp

--- sharkpitch/mainapp/urls.py
+++ sharkpitch/mainapp/urls.py
@@ -2,7 +2,20 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views 
 urlpatterns = [
+    path('',views.home,name='home'),
     path('investor_landing',views.Investor_landing,name='Investor_landing'),
     path('Investor_profile',views.Investor_profile,name='Investor_profile'),
-    path('',views.User_profile,name='User_profile'),
+    path('User_profile',views.User_profile,name='User_profile'),
+    path('login',views.login,name='login'),
+    path('signup',views.signup,name='signup'),
+    path('verification1',views.verification1,name='verification1'),
+    path('verification2',views.verification2,name='verification2'),
+    path('verification3',views.verification3,name='verification3'),
+    path('verification4',views.verification4,name='verification4'),
+    path('verification5',views.verification5,name='verification5'),
+    path('verification6',views.verification6,name='verification6'),
+    path('verification7',views.verification7,name='verification7'),
+    path('verification8',views.verification8,name='verification8'),
+    path('verification9',views.verification9,name='verification9'),
+    path('verification10',views.verification10,name='verification10'),
 ]
--- sharkpitch/mainapp/views.py
+++ sharkpitch/mainapp/views.py
@@ -4,9 +4,31 @@ from django.shortcuts import render
 
 def Investor_landing(request):
     return render(request,'investor_landing.html')
-
 def Investor_profile(request):
     return render(request,'investor_profile.html')
-
 def User_profile(request):
     return render(request,'user_profile.html')
+def login(request):
+    return render(request,'login.html')
+def signup(request):
+    return render(request,'sign_up_page.html')
+def verification1(request):
+    return render(request,'startup_verification_page1.html')
+def verification2(request):
+    return render(request,'startup_verification_page2.html')
+def verification3(request):
+    return render(request,'startup_verification_page3.html')
+def verification4(request):
+    return render(request,'startup_verification_page4.html')
+def verification5(request):
+    return render(request,'startup_verification_page5.html')
+def verification6(request):
+    return render(request,'startup_verification_page6.html')
+def verification7(request):
+    return render(request,'startup_verification_page7.html')
+def verification8(request):
+    return render(request,'startup_verification_page8.html')
+def verification9(request):
+    return render(request,'startup_verification_page9.html')
+def verification10(request):
+    return render(request,'startup_verification_page10.html')
