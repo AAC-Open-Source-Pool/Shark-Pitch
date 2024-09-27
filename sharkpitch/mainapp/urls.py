10f338347c7326a5cb8f0ae3cb30331064abffc3
vaishnavy1430 <vyshuchepuri1424@gmail.com>
2024-09-27 16:27:55 +0530

    inf change

--- sharkpitch/mainapp/urls.py
+++ sharkpitch/mainapp/urls.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('home',views.home,name='home'),
+    path('Interested_page',views.Intrested_page,name='intrested_page'),
+    path('',views.Startup_info,name='startup_info'),
 ]
--- sharkpitch/mainapp/views.py
+++ sharkpitch/mainapp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'homepage.html')
+
+def Intrested_page(request):
+    return render(request,'Interested_page.html')
+
+def Startup_info(request):
+    return render(request,'Startup_info.html')
