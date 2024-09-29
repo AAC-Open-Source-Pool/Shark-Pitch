afba9f375a3e9160cfbee14ad1a65736ff2271cd
Nagasree19 <kothakondanagasree8@gmail.com>
2024-09-29 12:52:52 +0530

    startup_homepage

--- sharkpitch/mainapp/urls.py
+++ sharkpitch/mainapp/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views 
 urlpatterns = [
-    path('',views.home,name='home')
+    path('home',views.home,name='home'),
+    path('',views.Startup_home,name='Startup_home'),
 ]
--- sharkpitch/mainapp/views.py
+++ sharkpitch/mainapp/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'homepage.html')
+def Startup_home(request):
+    return render(request,'Startup_home.html')
