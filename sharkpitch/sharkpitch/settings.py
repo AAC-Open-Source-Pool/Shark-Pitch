42beb18f4159bbb655263513878f5d68fab57a4c
Amudala Premendar Reddy <premamudala2@gmail.com>
2024-10-02 00:14:32 +0530

    integration

--- sharkpitch/sharkpitch/settings.py
+++ sharkpitch/sharkpitch/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'sharkpitch',
+    'mainapp',
 ]
 
 MIDDLEWARE = [
@@ -130,9 +130,12 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
+
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
 # Default primary key field type
+
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
