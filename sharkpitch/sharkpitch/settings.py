041622e8a6db77c19459833f7be8546232bebe51
Raag-24 <raag.chekuri@gmail.com>
2024-10-07 17:38:59 +0530

    migrations successful in postgres

--- sharkpitch/sharkpitch/settings.py
+++ sharkpitch/sharkpitch/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'mainapp.apps.MainappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
     'sharkpitch',
 ]
 
